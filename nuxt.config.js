691607ecd47d8ca1695c26445f3768b918487df2
Futa Ogawa <ogawa0071@gmail.com>
2019-01-22 00:47:02 +0900

    Fix default settings.

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,13 +7,13 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'Black Inc.',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'stylesheet', href: 'https://use.typekit.net/ybd1taf.css' }]
   },
 
   /*
